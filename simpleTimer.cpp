0add6af935a6ca24b30adf725937cfe75fab3ce0
Konstantin Gorbenko <kyb.6.granei@yandex.ru>
2024-06-24 23:48:35 +0300

    SimpleTimer32

--- simpleTimer.cpp
+++ simpleTimer.cpp
@@ -23,6 +23,11 @@ simpleTimer::simpleTimer (int32_t nprd)
   start_int();
 }
 
+void simpleTimer::setPrd(uint32_t nprd)
+{
+  prd = (uint64_t)nprd;
+}
+
 void simpleTimer::start_int() 
 {
   tmr = millis();
@@ -36,7 +41,51 @@ void simpleTimer::stop()
 
 bool simpleTimer::ready() 
 {
-  if ( !(tmr == 0ULL) && ( (millis() - tmr) >= prd ) )
+  if ( (tmr != 0ULL) && ( (millis() - tmr) >= prd ) )
+  {
+    start_int();
+    return true;
+  }
+
+  return false;
+}
+
+simpleTimer32::simpleTimer32 () 
+{
+
+}
+
+simpleTimer32::simpleTimer32 (uint32_t nprd) 
+{
+  prd = nprd;
+  start_int();
+}
+
+simpleTimer32::simpleTimer32 (int32_t nprd) 
+{
+  prd = nprd;
+  start_int();
+}
+
+void simpleTimer32::setPrd(uint32_t nprd)
+{
+  prd = nprd;
+}
+
+void simpleTimer32::start_int() 
+{
+  tmr = millis();
+  if (tmr == 0UL) tmr = 1UL;
+}
+
+void simpleTimer32::stop() 
+{
+  tmr = 0UL;
+}
+
+bool simpleTimer32::ready() 
+{
+  if ( (tmr != 0UL) && ( (millis32() - tmr) >= prd ) )
   {
     start_int();
     return true;
--- simpleTimer.h
+++ simpleTimer.h
@@ -2,6 +2,7 @@
 #include <cstdint>
 
 uint64_t millis(void);
+uint32_t millis32(void);
 
 class simpleTimer 
 {
@@ -10,11 +11,28 @@ class simpleTimer
     simpleTimer (uint64_t);
     simpleTimer (uint32_t);
     simpleTimer (int32_t);
+    void setPrd(uint32_t);
     void start_int();
     void stop();
     bool ready();
 
   private:
-      uint64_t tmr;
-      uint64_t prd;
+    uint64_t tmr;
+    uint64_t prd;
+};
+
+class simpleTimer32
+{
+  public:
+    simpleTimer32();
+    simpleTimer32 (uint32_t);
+    simpleTimer32 (int32_t);
+    void setPrd(uint32_t);
+    void start_int();
+    void stop();
+    bool ready();
+
+  private:
+    uint32_t tmr;
+    uint32_t prd;
 };
