05653c7b6acf34b76c58906e40c41a7c139a6c70
Konstantin Gorbenko <kyb.6.granei@yandex.ru>
2025-10-03 23:42:15 +0300

    update table generator

--- gen_table_10x_4096.py
+++ gen_table_10x_4096.py
@@ -4,8 +4,8 @@ rt = 100000;
 r  = 100000;
 
 print("const int16_t NTC_100Kx10 [] = {")
-for i in range(1, 4095):
+for i in range(613, 3741):
     res = 10*(1.0 / ((math.log(r / rt / (4095.0 / i - 1.0)) / 3950) + 1.0 / (25 + 273.15)) - 273.15)
     res = round(res)
-    print ('/*', i, '*/', res)
-print ("}")
+    print ('/*', i, '*/', res, ',')
+print ("};")
